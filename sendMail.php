d8097a7725e21a9baffcb0bc7e1a070a28dd35c6
Nathan J Lichtenstein <njlichtenstein@gmail.com>
2014-10-25 20:17:03 -0400

    validation in URL

--- sendMail.php
+++ sendMail.php
@@ -5,14 +5,19 @@
 	$message = htmlspecialchars($_POST["message"]);
 	
 	if(isset($senderName)&&isset($email)&&isset($message)) {
-	$sendMessage = "";
-	$sendMessage .= "Email from $senderName ($email)";
-	$sendMessage .= "\n";
-	$sendMessage .= "$message";
-	
-	mail("ritnexthop@gmail.com","Contact Form Submission",$sendMessage);
-	
-	header( 'Location: http://nh.brucedewald.com' ) ;
+		$sendMessage = "";
+		$sendMessage .= "Email from $senderName ($email)";
+		$sendMessage .= "\n\n";
+		$sendMessage .= "$message";
+		
+		if(mail("ritnexthop@gmail.com","Contact Form Submission",$sendMessage)) {
+			$status = "SUCCESS";
+		}
+		else {
+			$status = "FAIL";
+		}
+		
+		header( "Location: http://nh.brucedewald.com&status=$status" );
 	}
 ?>
 	
