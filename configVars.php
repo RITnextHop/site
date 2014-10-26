7194b8b2c4d518cb178878fb6405ba196620ba6c
Nathan J Lichtenstein <njlichtenstein@gmail.com>
2014-10-25 23:43:33 -0400

    Initial commit

---
+++ configVars.php
@@ -0,0 +1,18 @@
+<?php
+//president
+$president_fullName = "Nathan J. Lichtenstein";
+$president_email = "njl3451@rit.edu";
+$president_majorYear = "3rd Year ANSA";
+//vice president
+$vicePresident_fullName = "Bruce DeWald";
+$vicePresident_email = "Bruce.S.DeWald@rit.edu";
+$vicePresident_majorYear = "3rd Year ANSA";
+//treasurer
+$treasurer_fullName = "Jeffrey Kaiser";
+$treasurer_email = "jdk1489@rit.edu";
+$treasurer_majorYear = "3rd Year ANSA";
+//advisor
+$advisor_fullName = "Garret Arcoraci";
+$advisor_email = "gpavks@rit.edu";
+$advisor_Position = "IST Faculty";
+?>
