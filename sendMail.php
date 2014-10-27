952a7ccb7d292757c72794b915ccf803d1218583
Bruce DeWald <bsd5370@rit.edu>
2014-10-27 02:35:41 -0400

    php error

--- sendMail.php
+++ sendMail.php
@@ -1,51 +1,51 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		$senderName = $_POST["senderName"];
+		$email = $_POST["email"];
+		$message = htmlspecialchars($_POST["message"]);
 
-	$senderName = $_POST["senderName"];
-	$email = $_POST["email"];
-	$message = htmlspecialchars($_POST["message"]);
+	// Validate Name
+	if (empty($senderName)) {
+			$nameErr = "Name is required";
+	}else{
+			$valid_name = true;
+	}
 
-// Validate Name
-if (empty($senderName)) {
-		$nameErr = "Name is required";
-}else{
-		$valid_name = true;
+	// Validate email
+	if (empty($email)) {
+	    $emailErr = "Email is required";
+	} else if (!(preg_match("/^[a-zA-Z0-9\-_]+(\.[a-zA-Z0-9\-_]+)*@[a-z0-9]+(\-[a-z0-9]+)*(\.[a-z0-9]+(\-[a-z0-9]+)*)*\.[a-z]{2,4}$/", $email))){
+	    $emailErr = "Valid email is required";
+	} else {
+	    $valid_email = true;
+	}
+
+	// Validate message
+	if (empty($message)) {
+	    $messageErr = "Message is required";
+	}else{
+	    $valid_message = true;
+	}
+
+
+	// Send Message
+	if ($valid_name && $valid_email && $valid_message) {
+			$sendMessage = "";
+			$sendMessage .= "Email from $senderName ($email)";
+			$sendMessage .= "\n\n";
+			$sendMessage .= "$message";
+
+			if(mail("ritnexthop@gmail.com","Contact Form Submission",$sendMessage)) {
+				$status = "SUCCESS";	//eventually going to have some kind of confirmation after send
+			}
+			else {
+				$status = "FAIL";		//eventually going to have some kind of alert if send is a fail
+			}
+
+			header( "Location: http://nh.brucedewald.com?status=$status" );
+	}
+	else {
+		header( "Location: http://nh.brucedewald.com?status=$status");
+	}
 }
-
-// Validate email
-if (empty($email)) {
-    $emailErr = "Email is required";
-} else if (!(preg_match("/^[a-zA-Z0-9\-_]+(\.[a-zA-Z0-9\-_]+)*@[a-z0-9]+(\-[a-z0-9]+)*(\.[a-z0-9]+(\-[a-z0-9]+)*)*\.[a-z]{2,4}$/", $email))){
-    $emailErr = "Valid email is required";
-} else {
-    $valid_email = true;
-}
-
-// Validate message
-if (empty($message)) {
-    $messageErr = "Message is required";
-}else{
-    $valid_message = true;
-}
-
-
-// Send Message
-if ($valid_name && $valid_email && $valid_message) {
-		$sendMessage = "";
-		$sendMessage .= "Email from $senderName ($email)";
-		$sendMessage .= "\n\n";
-		$sendMessage .= "$message";
-
-		if(mail("ritnexthop@gmail.com","Contact Form Submission",$sendMessage)) {
-			$status = "SUCCESS";	//eventually going to have some kind of confirmation after send
-		}
-		else {
-			$status = "FAIL";		//eventually going to have some kind of alert if send is a fail
-		}
-
-		header( "Location: http://nh.brucedewald.com?status=$status" );
-}
-else {
-	header( "Location: http://nh.brucedewald.com?status=$status");
-}
-
 ?>
