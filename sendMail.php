8799bdf7891f0f113c18e764c568df768a1a5df4
Bruce DeWald <bsd5370@rit.edu>
2014-10-27 01:53:13 -0400

    added contact form validation

--- sendMail.php
+++ sendMail.php
@@ -1,24 +1,70 @@
 <?php
-	
+
 	$senderName = $_POST["senderName"];
 	$email = $_POST["email"];
 	$message = htmlspecialchars($_POST["message"]);
-	
+
+
+    $response=array('error'=>0,'info'=>null);
+
+    $values=array
+    (
+        'contact-user-name'     => $_POST['contact-user-name'],
+        'contact-user-email'	=> $_POST['contact-user-email'],
+        'contact-message'       => $_POST['contact-message']
+    );
+
+	if(isEmpty($values['$senderName']) || strcmp($values['senderName'],_def_data_name)==0)
+    {
+        $response['error']=1;
+        $response['info'][]=array('fieldId'=>'senderName','message'=>_msg_invalid_data_name);
+    }
+
+  if(!validateEmail($values['$email']) || strcmp($values['email'],_def_data_email)==0)
+    {
+        $response['error']=1;
+        $response['info'][]=array('fieldId'=>'email','message'=>_msg_invalid_data_email);
+    }
+
+  if(isEmpty($values['$message']) || strcmp($values['message'],_def_data_message)==0)
+    {
+        $response['error']=1;
+        $response['info'][]=array('fieldId'=>'message','message'=>_msg_invalid_data_message);
+    }
+
+
+	if($response['error']==1) createResponse($response);
+
 	if(isset($senderName)&&isset($email)&&isset($message)) {
 		$sendMessage = "";
 		$sendMessage .= "Email from $senderName ($email)";
 		$sendMessage .= "\n\n";
 		$sendMessage .= "$message";
-		
+
 		if(mail("ritnexthop@gmail.com","Contact Form Submission",$sendMessage)) {
 			$status = "SUCCESS";	//eventually going to have some kind of confirmation after send
 		}
 		else {
 			$status = "FAIL";		//eventually going to have some kind of alert if send is a fail
 		}
-		
+
 		header( "Location: http://nh.brucedewald.com?status=$status" );
 	}
+
+	function isEmpty($value)
+    {
+        return(!(bool)mb_strlen($value));
+    }
+
+	function validateEmail($email)
+    {
+        if(!preg_match('/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/',$email,$result)) return(false);
+        else return(true);
+    }
+
+		function createResponse($response)
+    {
+        echo json_encode($response);
+        exit;
+    }
 ?>
-	
-	
