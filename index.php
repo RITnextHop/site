f614c0eb511573dee89532b7c258a2150db1f1af
Bruce DeWald <bsd5370@rit.edu>
2014-10-27 17:20:55 -0400

    edited formatting of governance section

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ if(window.mobilecheck()) {
 					<ul>
 						<li>President: <a href="mailto:<?php echo $president_email; ?>"><?php echo $president_fullName; ?></a><br /><?php echo $president_majorYear; ?></li>
 						<li>Vice President: <a href="mailto:<?php echo $vicePresident_email; ?>"><?php echo $vicePresident_fullName; ?></a><br /><?php echo $vicePresident_majorYear; ?></li>
-						<li>Treasurer: <a href="mailto:<? echo $treasurer_email; ?>"><? echo $treasurer_fullName; ?></a><br /><?php echo $treasurer_majorYear; ?></li>
+						<li>Treasurer: <a href="mailto:<? echo $treasurer_email; ?>"><? echo $treasurer_fullName; ?></a><br /><span class="major"><?php echo $treasurer_majorYear; ?></span></li>
 					</ul>
 					<h2>Advisor</h2>
 					<ul>
