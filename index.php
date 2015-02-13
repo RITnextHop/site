8cabf86918ff2f2c5c9a976e43f6381cc6f1d609
Bruce DeWald <bsd5370@rit.edu>
2015-02-13 12:27:18 -0500

    Changed Next Hop to NextHop everywhere

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 	include "events.php";
 ?>
 <!doctype HTML>
-<!--RIT Next Hop Club Site -->
+<!--RIT NextHop Club Site -->
 <head>
 <link rel="stylesheet" id="base" type="text/css" href="style/base.css">
 <link rel="stylesheet" id="medium" type="text/css" href="style/desktop.css">
 <link href='http://fonts.googleapis.com/css?family=Noto+Sans:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
 <link href="style/font-awesome-4.2.0/css/font-awesome.min.css" rel="stylesheet">
 <meta name="viewport" content="initial-scale=1">
-<title>Next Hop</title>
+<title>NextHop</title>
 <script>
 window.mobilecheck = function() {
   var check = false;
@@ -25,16 +25,16 @@ if(window.mobilecheck()) {
 <body>
 	<div class="section" id="topBar">
 		<div class="sectionInner">
-			<img style="margin:0 auto;" src="http://placehold.it/600x300&text=Next Hop" />
-			<span id="nextHop">Next Hop</span>
+			<img style="margin:0 auto;" src="http://placehold.it/600x300&text=NextHop" />
+			<span id="nextHop">NextHop</span>
 		</div>
 	</div>
 	<div class="section" id="about">
 		<div class="sectionInner text">
-			<h1 class="sectionHeader"><i class="fa fa-question-circle"></i> What is Next Hop?</h1>
+			<h1 class="sectionHeader"><i class="fa fa-question-circle"></i> What is NextHop?</h1>
 			<div class="padLeft">
-				<p>Next Hop is an organization at the Rochester Institute of Technology geared towards students interested in the fields of networking and systems administration. The name Next Hop comes from a networking term that describes the next router along a path to a given destination. The club serves two core functions: community building and knowledge development.
-				<p>The community building aspect of Next Hop is geared towards bringing students together from multiple disciplines who share a core common interest set. Knowledge development is focused on building core and advanced level skills through a variety of workshops and presentations with topics ranging from basic skills to emerging technologies. Knowledge development spans both academic and professional knowledge spheres. </p>
+				<p>NextHop is an organization at the Rochester Institute of Technology geared towards students interested in the fields of networking and systems administration. The name NextHop comes from a networking term that describes the next router along a path to a given destination. The club serves two core functions: community building and knowledge development.
+				<p>The community building aspect of NextHop is geared towards bringing students together from multiple disciplines who share a core common interest set. Knowledge development is focused on building core and advanced level skills through a variety of workshops and presentations with topics ranging from basic skills to emerging technologies. Knowledge development spans both academic and professional knowledge spheres. </p>
 			</div>
 		</div>
 	</div>
@@ -86,7 +86,7 @@ if(window.mobilecheck()) {
 	</div>
 	<div class="section" id="footer">
 		<div class="sectionInner">
-			&copy; 2014 Next Hop
+			&copy; 2014 NextHop
 		</div>
 	</div>
 </body>
