a8d07915f1a039e18872877485e7f444cb82a3d4
Bruce DeWald <bsd5370@rit.edu>
2015-02-20 11:50:49 -0500

    room change

--- events.php
+++ events.php
@@ -4,7 +4,7 @@
 	}
 
 	$Feb26 = new Event();
-	$Feb26->location = 'Golisano, 70-2400';
+	$Feb26->location = 'Golisano, 70-1455';
 	$Feb26->date = 'Thursday, February 26 2015';
 	$Feb26->time = '7:00 P.M. - 9:00 P.M.';
 	$Feb26->description = "Come learn about cool things like SDN, Ansible, Scapy, and Linux FACLs! Please RSVP at <a href='http://goo.gl/forms/BT2s5pkPMa'>this link</a>";
