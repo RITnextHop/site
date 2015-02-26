d0ba1e133f678f7e558432b0c834340ddc6d65c4
Bruce DeWald <bsd5370@rit.edu>
2015-02-25 23:46:44 -0500

    fixing header

--- config/header.php
+++ config/header.php
@@ -1,4 +1,4 @@
-<div class="section" id="topBar">
+<div id="topBar">
   <div class="sectionInner">
     <div style="background-color:white;border-radius:9999px;margin: 20px 0;">
       <a href="/"><img style="margin: 0 auto;overflow:auto;width:550px;" src="/images/NextHopLogo.png"></a>
@@ -6,7 +6,7 @@
     <span id="nextHop">NextHop</span>
   </div>
 </div>
-<div class="section" id="menubarouter">
+<div id="menubarouter">
   <div id="menubar">
     <a href="/" class="menubarlink" id="homenav"><i class="fa fa-home" ></i> Home</a>
     <a href="/live" class="menubarlink" id="livenav"><i class="fa fa-video-camera"></i> Live Stream</a>
