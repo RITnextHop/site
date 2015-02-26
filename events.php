d9ba01c73437002e52599f929b5727b4ce22506e
Bruce DeWald <bsd5370@rit.edu>
2015-02-25 23:44:27 -0500

    site cleanup

--- events.php
+++ events.php
@@ -36,21 +36,7 @@
     </script>
   </head>
   <body id="events">
-  	<div class="section" id="topBar">
-  		<div class="sectionInner">
-  			<div style="background-color:white;border-radius:9999px;margin: 20px 0;">
-  				<a href="/"><img style="margin: 0 auto;overflow:auto;width:550px;" src="/images/NextHopLogo.png"></a>
-  			</div>
-  			<span id="nextHop">NextHop</span>
-  		</div>
-  	</div>
-  	<div class="section" id="menubarouter">
-  		<div id="menubar">
-  			<a href="/" class="menubarlink" id="homenav"><i class="fa fa-home" ></i> Home</a>
-  			<a href="/live" class="menubarlink" id="livenav"><i class="fa fa-video-camera"></i> Live Stream</a>
-        <a href="/events" class="menubarlink" id="eventsnav"><i class="fa fa-calendar"></i> Events</a>
-  		</div>
-  	</div>
+    <?php include("config/header.php"); ?>
     <div class="section" id="events-upcoming">
       <div class="sectionInner text">
   			<h1 class="sectionHeader"><i class="fa fa-calendar"></i> Upcoming Events</h1>
--- index.php
+++ index.php
@@ -35,21 +35,7 @@ if(window.mobilecheck()) {
 </script>
 </head>
 <body id="home">
-	<div class="section" id="topBar">
-		<div class="sectionInner">
-			<div style="background-color:white;border-radius:9999px;margin: 20px 0;">
-				<a href="/"><img style="margin: 0 auto;overflow:auto;width:550px;" src="/images/NextHopLogo.png"></a>
-			</div>
-			<span id="nextHop">NextHop</span>
-		</div>
-	</div>
-	<div class="section" id="menubarouter">
-		<div id="menubar">
-			<a href="/" class="menubarlink" id="homenav"><i class="fa fa-home" ></i> Home</a>
-			<a href="/live" class="menubarlink" id="livenav"><i class="fa fa-video-camera"></i> Live Stream</a>
-			<a href="/events" class="menubarlink" id="eventsnav"><i class="fa fa-calendar"></i> Events</a>
-		</div>
-	</div>
+	<?php include("config/header.php"); ?>
 	<div class="section" id="about">
 		<div class="sectionInner text">
 			<h1 class="sectionHeader"><i class="fa fa-question-circle"></i> What is NextHop?</h1>
@@ -117,11 +103,12 @@ if(window.mobilecheck()) {
 				</div>
 			</div>
 		</div>
-	</div>
-	<div class="section" id="footer">
-		<div class="sectionInner">
-			&copy; 2015 NextHop
+		<div class="section" id="footer-homepage">
+			<div class="sectionInner">
+				&copy; 2015 NextHop
+			</div>
 		</div>
 	</div>
+
 </body>
 </html>
