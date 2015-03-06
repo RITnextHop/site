6072477e8c8b2a6bc654c44d243f3b6310c7cd0a
Bruce DeWald <bsd5370@rit.edu>
2015-03-05 23:30:20 -0500

    added 3/12 event

--- config/events.php
+++ config/events.php
@@ -3,6 +3,13 @@
 		public $title,$location,$date,$time,$description;
 	}
 
+	$Mar_12_2015 = new Event();
+	$Mar_12_2015->title = 'DHCP/DNS Build-It-Night';
+	$Mar_12_2015->location = 'Golisano, 70-2320';
+	$Mar_12_2015->date = 'Thursday, March 12, 2015';
+	$Mar_12_2015->time = '7:00 P.M. - 9:00 P.M.';
+	$Mar_12_2015->description = 'Learn about DHCP or DNS or refresh your knowledge! Please RSVP at <a href=\'https://docs.google.com/forms/d/182wgtyvxIcB2u_atbOBR4Plot-uvNTE40lWGNjH9eBk/viewform?c=0&w=1\'>this link</a>';
+
 	$Feb_26_2015 = new Event();
 	$Feb_26_2015->title = 'Tech Talks';
 	$Feb_26_2015->location = 'Golisano, 70-1455';
@@ -25,7 +32,7 @@
 	$Nov_06_2014->time = '6:30 P.M. - 7:30 P.M.';
 	$Nov_06_2014->description = 'Initial NextHop Meeting!';
 
-	$events = array($Feb_26_2015);
-	$pastevents = array($Nov_20_2014,$Nov_06_2014)
+	$events = array();
+	$pastevents = array($Feb_26_2015,$Nov_20_2014,$Nov_06_2014)
 
 ?>
