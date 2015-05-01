678d3c06c4a4238e5bb208e8d9e975933f425848
Jeffrey Kaiser <jeffreykaiser@jdkrmbp.local>
2015-04-30 21:51:44 -0400

    comment error

--- index.php
+++ index.php
@@ -38,8 +38,8 @@ if(window.mobilecheck()) {
 	<?php include("config/header.php"); ?>
 	<div class="section" id="about">
 		<div class="sectionInner text">	
-		<!--<p><strong><i class="fa fa-clock-o fa-2x"></i> Reminder that our PXE boot build it night is tonight, Thursday 4/30 at 7pm in 70-2320.  Hope to see you there!</strong></p>-->
-		<hr>
+		<!--<p><strong><i class="fa fa-clock-o fa-2x"></i> Reminder that our PXE boot build it night is tonight, Thursday 4/30 at 7pm in 70-2320.  Hope to see you there!</strong></p>
+		<hr>-->
 			<h1 class="sectionHeader"><i class="fa fa-question-circle"></i> What is NextHop?</h1>
 			<div class="padLeft">
 			    <p>NextHop is a club at the Rochester Institute of Technology (RIT) focused on networking and systems administration. NextHop is all about moving information and managing different services that run on a network.</p>
