09376705d0f5d3fe537b68d4daaa14bed89521c8
Jeffrey Kaiser <jdk1489@rit.edu>
2015-05-27 11:43:57 -0400

    Changed eboard info

--- config/configVars.php
+++ config/configVars.php
@@ -1,16 +1,16 @@
 <?php
 //president
-$president_fullName = "Nathan J. Lichtenstein";
-$president_email = "njl3451@rit.edu";
-$president_majorYear = "3rd Year ANSA";
+$president_fullName = "Jeffrey Kaiser";
+$president_email = "jdk1489@rit.edu";
+$president_majorYear = "4th Year ANSA";
 //vice president
 $vicePresident_fullName = "Bruce DeWald";
 $vicePresident_email = "Bruce.S.DeWald@rit.edu";
-$vicePresident_majorYear = "3rd Year ANSA";
+$vicePresident_majorYear = "4th Year ANSA";
 //treasurer
-$treasurer_fullName = "Jeffrey Kaiser";
-$treasurer_email = "jdk1489@rit.edu";
-$treasurer_majorYear = "3rd Year ANSA";
+$treasurer_fullName = "Tom Gorski";
+$treasurer_email = "tng8939@rit.edu";
+$treasurer_majorYear = "4th Year ANSA";
 //advisor
 $advisor_fullName = "Garret Arcoraci";
 $advisor_email = "gpavks@rit.edu";
