e68a96c9b9a0f509b6acbae0c2c21b886367fa95
Jeff Kaiser <jdk1489@rit.edu>
2016-03-16 17:31:32 -0400

    Fixed youtube embed

--- live.php
+++ live.php
@@ -38,7 +38,7 @@ if(window.mobilecheck()) {
 				<div class="sectionInner text">
 					<h1 class="sectionHeader"><i class="fa fa-video-camera"></i> Live Stream - Tech Talks 3/16/16</h1>
 					<div class="video">
-						<iframe width="560" height="315" src="https://www.youtube.com/embed/w_o-b7IZVhA" frameborder="0" allowfullscreen></iframe><br />
+						<iframe width="500" height="315" src="https://www.youtube.com/embed/w_o-b7IZVhA" frameborder="0" allowfullscreen></iframe><br />
 						View the slides for the Docker presentation <a href="http://nexthop.network/docs/docker.pdf" target="_blank">here</a>.<br />
             View the slides for the IST Labs Infrastructure presentation<a href="https://docs.google.com/a/g.rit.edu/presentation/d/1FwhIYU84c8dpOKqDdrWKZeSn7QzvKEl9pIgzmwgRl8o/edit?usp=sharing" target="_blank"> here</a>.<br />
 						For older live streams, please see the <a href="events.php">events</a> page.
